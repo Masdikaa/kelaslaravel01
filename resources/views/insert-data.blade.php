f476939ab19a54b2a28f74552a5869592d406d84
Masdikaa <masdikailhanmansiz@gmail.com>
2023-09-13 08:13:52 +0700

    Form Input Data

--- resources/views/insert-data.blade.php
+++ resources/views/insert-data.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Insert Data</h1>
+    <div class="container mt-5">
+        <div class="card border-secondary">
+            <div class="card-header text-center">
+                <h3>Insert Data</h3>
+            </div>
+            <div class="card-body pt-4">
+                {{-- FORM INPUT --}}
+                <form class="mt-5" method="POST" action="store-data">
+
+                    @csrf
+                    <div class="form-group my-2">
+                        <label class="font-weight-bold">No Induk</label>
+                        <input type="text" name="no_induk" class="form-control">
+                    </div>
+
+                    <div class="form-group my-2">
+                        <label class="font-weight-bold">Nama</label>
+                        <input type="text" name="nama" class="form-control">
+                    </div>
+
+                    <div class="form-group my-2">
+                        <label class="font-weight-bold">Alamat</label>
+                        <input type="text" name="alamat" class="form-control">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
 @endsection
